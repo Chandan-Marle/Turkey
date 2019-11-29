f80e5ed6d8dd62db5bba62d14525bd3c68e870b9
Chandan-Marle <chandanmarlex12@gmail.com>
2019-11-28 19:40:40 -0700

    Finish Turckey Challenge

---
+++ code.js
@@ -0,0 +1,27 @@
+originalTurkey = "<img src='Turkey.png' style='left: 300px; top: 100px; position: absolute; height: 100px; width: 100px;' onclick='create()'>"
+document.write(originalTurkey);
+k = 1
+function create(){
+    for (let i = 0; i < k; i++) {
+        let left = 1700*Math.random();
+        let top = 880*Math.random();
+        left = Math.round(left);
+        top = Math.round(top);
+        document.write(originalTurkey);
+        let newTurkey = "<img src='Turkey.png' onclick='create()' style='left:" + left + "px; top:" + top + "px; position: absolute; height: 100px; width: 100px;'>";
+        document.write(newTurkey);
+        document.write("<body onkeypress='reset()'></body>");
+    }
+    k = k*2
+}
+
+
+function reset() {
+    console.log('Active')
+    if (event.keyCode === 99) {
+        document.body.innerHTML = '';
+        document.write("<body onkeypress='reset()'></body>");
+        document.write(originalTurkey);
+    }    
+}
+
